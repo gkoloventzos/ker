3dc74ec488ea5146bac2cfa4efb147f170dac8d4
Georgios Koloventzos <g.koloventzos@gmail.com>
2016-04-07 09:20:33 -0600

    vgic changes in 4.0

--- virt/kvm/arm/vgic.c
+++ virt/kvm/arm/vgic.c
@@ -2454,13 +2454,13 @@ int kvm_irq_map_chip_pin(struct kvm *kvm, unsigned irqchip, unsigned pin)
 int kvm_set_irq(struct kvm *kvm, int irq_source_id,
 		u32 irq, int level, bool line_status)
 {
-	unsigned int spi = irq + VGIC_NR_PRIVATE_IRQS;
+//	unsigned int spi = irq + VGIC_NR_PRIVATE_IRQS;
 
 	trace_kvm_set_irq(irq, level, irq_source_id);
 
 	BUG_ON(!vgic_initialized(kvm));
 
-	return kvm_vgic_inject_irq(kvm, 0, spi, level);
+	return kvm_vgic_inject_irq(kvm, 0, irq, level);
 }
 
 /* MSI not implemented yet */
